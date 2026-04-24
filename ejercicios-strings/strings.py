6ba90d637b27d9cf4b52f3c12e85637fa5f4024b
merka777 <juanestebanmonsalvesalinas1102@gmail.com>
2026-04-23 22:12:00 -0500

    primeros-ejercicios-strings

---
+++ ejercicios-strings/strings.py
@@ -0,0 +1,24 @@
+arroba = "@mcpenyair" 
+print (arroba.removeprefix("@")) 
+
+
+person_name = "charlie" 
+print (person_name.title()) 
+print (person_name.upper()) 
+print(person_name.lower()) 
+
+
+famous_person = "Warren Buffet" 
+message = (f'''{famous_person} once said,"It is far better to buy a wonderful company at a fair price than a fair company at a wonderful price".''') 
+print(message) 
+
+
+name = "  \n\tWarren Buffet   " 
+real_name = name.strip() 
+print (real_name) 
+
+ 
+
+filename = "python_notes.txt" 
+simplefile = filename.removesuffix(".txt") 
+print (simplefile) 
