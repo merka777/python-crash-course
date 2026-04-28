e03956d9bc57925188af1f256f1fd1c836b54fe6
merka777 <juanestebanmonsalvesalinas1102@gmail.com>
2026-04-28 07:43:33 -0500

    introducción-listas

---
+++ ejercicios-listas/listas.py
@@ -0,0 +1,20 @@
+money_makers = ["Warren Buffet", "Elon Musk", "Jeff Bezos", "Bill Gates", "Mark Zuckerberg"]
+
+print(money_makers[-5])
+
+
+names = ["JD", "JR", "Chomelo", "Copo"]
+
+print(f"Hi! You're here because you are my friend, salute! {names[0]}")
+print(f"Hi! You're here because you are my friend, salute! {names[-3]}")
+
+print(f"Hi! You're here because you are my friend, salute! {names[2]}")
+print(f"Hi! You're here because you are my friend, salute! {names[-1]}")
+ 
+
+cars = ["Cybertruck", "Urus", "MT-09", "NKD"]
+
+print(f"The {cars[0]} is a very fancy car, I'v never seen this car in real life ")
+print(f"The {cars[-3]} is the car of my dreams. I loved it")
+print (f"The {cars[2]} is the motorcycle of my dreams. What a ride!")
+print (f"The {cars[-1]} was my first vehicule. I'm proud of this")
