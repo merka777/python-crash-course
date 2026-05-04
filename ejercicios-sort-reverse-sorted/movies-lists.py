ab4072afd6efa5ea8feacbc713ef7ce07a5a5262
merka777 <juanestebanmonsalvesalinas1102@gmail.com>
2026-05-03 22:56:31 -0500

    functions-of-lists

---
+++ ejercicios-sort-reverse-sorted/movies-lists.py
@@ -0,0 +1,19 @@
+my_favorites_movies = ["Spider-man: Across the Spider-verse", "Forrest Gump", "The Wolf of Wall Street", "Dark Knight", "Up"]
+
+print("\n", sorted(my_favorites_movies))
+
+print("\n", my_favorites_movies)
+
+my_favorites_movies.reverse()
+
+print("\n", my_favorites_movies)
+
+my_favorites_movies.reverse()
+
+print("\n", my_favorites_movies)
+
+my_favorites_movies.sort()
+
+print("\n", my_favorites_movies)
+
+print("\n",sorted(my_favorites_movies, reverse=True))
---
+++ ejercicios-sort-reverse-sorted/rambo.py
@@ -0,0 +1,34 @@
+places = ["New York", "Barcelona", "Medellín", "Paris", "Los Angeles"]
+
+print(places)
+
+print(sorted(places))
+
+print(places)
+
+print((sorted(places, reverse=True)))
+
+print("\n", places)
+
+
+places.reverse()
+
+print("\n", places)
+places.reverse()
+
+print("\n", places)
+
+places.sort()
+
+print("\n", places)
+
+places.sort(reverse=True)
+
+print("\n", places)
+
+print(len(places))
+
+
+
+
+
