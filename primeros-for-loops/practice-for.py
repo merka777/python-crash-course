d9c6e9d32b979b09fa6a8f6cc354dcac2f8b3dda
merka777 <juanestebanmonsalvesalinas1102@gmail.com>
2026-05-13 22:32:36 -0500

    ejerecicios-loops

---
+++ primeros-for-loops/practice-for.py
@@ -0,0 +1,22 @@
+#PIZZAS EXERCISE
+
+pizzas = ["Mexicana", "Pepperoni", "Criolla"]
+
+for pizza in pizzas:
+    print(f"Me gusta este sabor: {pizza}\n")
+
+print("La pizza es de mis platillos preferidos\nMe gusta la pizza como a Las Tortugas Ninja\n¡¡¡¡Me gusta mucho la pizza!!!\n\n")
+
+print("-----------------------------")
+
+#ANIMALS EXERCISE
+
+animals = ["Perro", "Gato", "Hamster"]
+
+for animal in animals:
+    print(f"Este animal es excelente para ser una mascota: {animal}\n")
+
+print("¡¡¡Estos animales tienen en comun que todos son domesticos, no hay ninguno salvaje u hostil!!!")
+
+
+
